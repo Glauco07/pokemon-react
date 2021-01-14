af587cc3c8894d30ca2e6ba632223c9e1d79cf2d
Glauco Borges <glaucohborges@gmail.com>
2021-01-14 14:11:34 -0300

    Project created

---
+++ src/pokemon.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Pokemon() {
+    return (
+        <div>
+            <h1>Uma foto qualquer!</h1>
+            <img src="https://picsum.photos/200/300"/>
+            <p>Essa é uma foto aleatória</p>
+        </div>
+    )
+}
+
+export default Pokemon
